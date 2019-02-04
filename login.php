9a81b0a4ce7cef09dc8a8f9a0717631edf026a74
noemieblt <noemieboillot5@hotmail.fr>
2019-02-04 17:15:53 +0100

    Update login.php

--- login.php
+++ login.php
@@ -12,11 +12,17 @@ try {           $bdd = new PDO('mysql:host=localhost;dbname=caa;charset=utf8', '
 	<title>login</title>
 </head>
 <body>
+    <br>
+    <br>
 	<form action="login.php" method="post">
 	Name: <input type="username" name="name"><br>
 	Password: <input type="password" name="password"><br>
+
+    <br>
 <input name="login" type="submit" >
 </form>
+    <br>
+    <br>
 </body>
 </html>
 
