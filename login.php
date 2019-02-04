9c0487fa4469837d013e4c6deec0809a8a7b80b9
noemieblt <noemieboillot5@hotmail.fr>
2019-02-04 16:47:35 +0100

    Update login.php

--- login.php
+++ login.php
@@ -12,3 +12,22 @@
 </form>
 </body>
 </html>
+
+<?php
+            
+        $identifiant = $_POST['username'] ;
+        $mdp = $_POST['password'] ;
+        $result = $bdd->query('SELECT * FROM idvd');
+        $donnee = $result->fetch();
+        while($donnee != null){
+            if($donnee['username']==$identifiant AND $donnee['password']==$mdp){
+                $user_id = $donnee['id'];
+                $_SESSION['identifiant'] = $identifiant;
+                $_SESSION['password'] = $mdp;
+                $_SESSION['id'] = $user_id;
+                $co=true;
+            }
+            $donnee = $result->fetch();
+        } 
+        $result->closeCursor();
+?>
