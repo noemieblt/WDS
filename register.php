2c161e99da161a7c654dc5364732f0560fa8059d
Benjamin L <benjamin.legrand@isen.yncrea.fr>
2019-02-04 17:00:35 +0100

    Update register.php

--- register.php
+++ register.php
@@ -4,4 +4,30 @@ try {           $bdd = new PDO('mysql:host=localhost;dbname=caa;charset=utf8', '
     catch (Exception $e)    {           die('Erreur : ' . $e->getMessage());        }
 
 ?>
-
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div id="global">
+		<div class="login-wrap">
+			<span class="login-title-form">
+				Inscrivez-vous !
+			</span>
+			<br>
+			<form method="POST" action="inscription.php" class="login-form" required >
+	  			<input class="login-input" type="text" name="username" placeholder="Identifiant" required>
+	  			<br>
+	  			<input class="login-input" type="password" name="password" placeholder="Mot de passe" required>
+	  			<br>
+	  			<input class="login-input" type="password" name="confirm-password" placeholder="Confirmer mot de passe" required>
+	  			<br><br>
+	  			<input class="submit-input" type="submit" name="validation" value="Valider">
+	  			<a class="lien" href="logout.php">Connexion</a>
+	  			<br>
+			</form>
+		</div>
+	</div> 
+</body>
+</html>
